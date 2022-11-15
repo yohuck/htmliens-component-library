f784bf399896446afa9e1a62781087d56c4fadce
Eric Lake <ericallenlake@gmail.com>
2022-11-14 20:17:53 -0700

    First button + storybook

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,3 +1,3 @@
 export const Button = () => {
-    return <button className="p-2 border border-slate-900">Hello World</button>
+    return <button className="shadow px-2 py-1 border hover:outline hover:outline-1 hover:outline-red-700  focus:outline-1 focus:outline-red-700 border-slate-900 rounded-md">Submit</button>
 };
