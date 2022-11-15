b5bbb69d4b873ba0814c285fa504dbaa06de0954
Eric Lake <ericallenlake@gmail.com>
2022-11-14 21:27:22 -0700

    Adds prettier + eslint + scripts

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,4 +1,6 @@
-export const Button = () => {
+import React from 'react';
+
+export const Button: React.FC = () => {
   return (
     <button className="shadow px-2 py-1 border hover:outline hover:outine-4 hover:outline-yellow-400 focus:outline focus:outline-4 focus:outline-yellow-400 active:outline active:outline-4 active:outline-offset-2 active:outline-orange-600 border-slate-900 rounded-md">
       Submit
