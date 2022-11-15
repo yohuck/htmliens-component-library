60aecfbb3b9faadd582576f4ee9d8516ad44b08a
Eric Lake <ericallenlake@gmail.com>
2022-11-14 20:56:47 -0700

    Add accessibility check to storybook

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,3 +1,7 @@
 export const Button = () => {
-    return <button className="shadow px-2 py-1 border hover:outline hover:outline-1 hover:outline-red-700  focus:outline-1 focus:outline-red-700 border-slate-900 rounded-md">Submit</button>
+  return (
+    <button className="shadow px-2 py-1 border hover:outline hover:outine-4 hover:outline-yellow-400 focus:outline focus:outline-4 focus:outline-yellow-400 active:outline active:outline-4 active:outline-offset-2 active:outline-orange-600 border-slate-900 rounded-md">
+      Submit
+    </button>
+  );
 };
