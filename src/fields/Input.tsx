9e58afd7e855db2cb794bbe348221030404c1742
Eric Lake <ericallenlake@gmail.com>
2022-11-28 17:09:53 -0700

    Adds onblur validation

--- src/fields/Input.tsx
+++ src/fields/Input.tsx
@@ -16,17 +16,26 @@ export const Input = React.forwardRef<
   const id = useContext(FieldContext);
   return (
     <input
-      className="p-2 success-input rounded-md shadow  border-slate-900 border-4 dark:border-yellow-500 dark:text-yellow-500 dark:bg-slate-900"
+      className="p-2 rounded-md shadow  border-slate-900 border-4 dark:border-yellow-500 dark:text-yellow-500 dark:bg-slate-900"
       ref={ref}
       id={id}
       onBlur={(e) => {
         const isValid = emailValidator.safeParse(e.target.value);
         if (!isValid.success) {
+            const errorText = isValid.error.issues[0].message;
             e.target.classList.remove('dark:border-yellow-500', 'dark:text-yellow-500');
-            e.target.classList.add('dark:border-red-500', 'dark:text-red-500');
+            e.target.classList.add('dark:border-red-500', 'dark:text-red-500', 'failure');
+            e.target.labels?.[0].firstChild?.after(` ${errorText}`);
+            e.target.labels?.[0].classList.add("dark:text-red-500");
+            e.target.labels?.[0].classList.add("failure");
         } else {
-            e.target.classList.remove('error-input');
-            e.target.classList.add('dark:border-yellow-500 dark:text-yellow-500');
+            e.target.classList.remove('dark:border-red-500', 'dark:text-red-500');
+            e.target.classList.add('dark:border-yellow-500', 'dark:text-yellow-500', 'success');
+            console.log(e.target.labels?.[0].childNodes[1]?.remove());
+            e.target.labels?.[0].classList.remove("dark:text-red-500");
+            e.target.labels?.[0].classList.add("dark:text-yellow-500");
+            e.target.labels?.[0].classList.remove("failure");
+            e.target.labels?.[0].classList.add("success");
         }
         }}
       {...props}
--- src/fields/Label.tsx
+++ src/fields/Label.tsx
@@ -6,7 +6,7 @@ export const Label = React.forwardRef<
   React.ComponentPropsWithoutRef<'label'>
 >((props, ref) => {
   const id = useContext(FieldContext);
-  return <label className="p-2 success dark:text-yellow-500"ref={ref} htmlFor={id} {...props} />;
+  return <label className="p-2 dark:text-yellow-500"ref={ref} htmlFor={id} {...props} />;
 });
 
 Label.displayName = 'Field.Label';
