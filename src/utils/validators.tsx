d58e0d329d80104cb1ad5656d8e663758d5ae953
Eric Lake <ericallenlake@gmail.com>
2022-11-30 21:47:56 -0700

    Adds postcss addon to storybook, range input styling, and conditional to validator that fixes label issue for different input types

--- src/utils/validators.tsx
+++ src/utils/validators.tsx
@@ -9,14 +9,14 @@ export const ValidatorFn = (
   const textValidator = z.string().min(1);
   const emailValidator = z.string().email();
   const passwordValidator = z.string().min(8);
-  const numberValidator = z.number().int().positive();
+  const numberValidator = z.string().min(1).regex(/[0-9]/);
   const telValidator = z.string().regex(/^(\+?1)?[2-9]\d{2}[2-9](?!11)\d{6}$/);
   const urlValidator = z.string().url();
   const dateValidator = z.date();
   const timeValidator = z.string().regex(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/);
   const colorValidator = z.string().regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
   const searchValidator = z.string().min(1);
-  const rangeValidator = z.number().int().positive();
+  const rangeValidator = z.string().min(1).regex(/[0-9]/);
   const fileValidator = z.string().min(1);
 
   const validators = {
@@ -59,9 +59,11 @@ export const ValidatorFn = (
       e.target.classList.add(
         'dark:border-yellow-500',
         'dark:text-yellow-500',
-        'success'
+        // 'success'
       );
-      e.target.labels?.[0].childNodes[1].remove();
+      if (e.target.labels?.[0].childNodes[1]) {
+        e.target.labels?.[0].childNodes[1].remove();
+      }
       e.target.labels?.[0].classList.remove('dark:text-red-500');
       e.target.labels?.[0].classList.add('dark:text-yellow-500');
       e.target.labels?.[0].classList.remove('failure');
